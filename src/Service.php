5d2ca960be91904323bfaee7a6577bd94e767357
Adam Balogh <baloghbh@gmail.com>
2015-06-28 18:33:28 +0000

    Update service

--- src/Service.php
+++ src/Service.php
@@ -2,19 +2,19 @@
 
 use Phprest\Service\Serviceable;
 use Phprest\Service\Configurable;
-use League\Container\Container;
+use League\Container\ContainerInterface;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
 class Service implements Serviceable
 {
     /**
-     * @param Container $container
+     * @param ContainerInterface $container
      * @param Configurable $config
      *
      * @return void
      */
-    public function register(Container $container, Configurable $config)
+    public function register(ContainerInterface $container, Configurable $config)
     {
         if ( ! $config instanceof Config) {
             throw new \InvalidArgumentException('Wrong Config object');
